2135360208a7e88fef3794b21b46f32994895a3e
Lewis <lewis.renfrew@linn.co.uk>
2025-07-01 16:03:09 +0100

    dont try and run dist test files

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,18 @@
 import type { JestConfigWithTsJest } from 'ts-jest';
 
 const config: JestConfigWithTsJest = {
-  preset: 'ts-jest/presets/default-esm',
-  testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.ts'],
-  transform: {
-    '^.+\\.ts$': ['ts-jest', { useESM: true }],
-      '^.+\\.js$': 'babel-jest',
+    preset: 'ts-jest/presets/default-esm',
+    testEnvironment: 'node',
+    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 
-  },
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
+    extensionsToTreatAsEsm: ['.ts'],
+    transform: {
+        '^.+\\.ts$': ['ts-jest', { useESM: true }],
+        '^.+\\.js$': 'babel-jest'
+    },
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1'
+    }
 };
 
 export default config;
