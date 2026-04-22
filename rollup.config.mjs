04545be50045f3746a730b367fa98825666c5ce2
Lewis Renfrew <lewis.renfrew@linn.co.uk>
2026-04-22 16:28:42 +0100

    node 24 fix

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,7 +4,7 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
-import pkg from './package.json' assert { type: 'json' };
+import pkg from './package.json' with { type: 'json' };
 
 export default [
     {
